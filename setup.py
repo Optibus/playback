d4d7d059b2935a966139130f02d543853a76d8c7
eitany <eitan@optibus.com>
2021-01-11 11:40:23 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup, find_packages
+import setuptools
 
-VERSION = "0.0.1"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-setup(
-    name='playback2',
-    version=VERSION,
+setuptools.setup(
+    name="playback-studio",
+    version="0.0.1",
     author="Optibus",
-    packages=find_packages(),
+    author_email="eitan@optibus.com",
+    description="Record your service operations in production and replay them locally at any time in a sandbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Optibus/playback",
+    packages=setuptools.find_packages(),
     install_requires=[
         'parse==1.6.6',
         'jsonpickle==0.9.3',
@@ -16,4 +21,10 @@ setup(
         'mock==2.0.0',
         'moto==1.3.13'
     ]},
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
 )
