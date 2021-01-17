78396e40bb891fef3a7639ca0ce8c00499777d77
eitany <eitan@optibus.com>
2021-01-17 16:59:41 +0200

    bump to version 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.install import install
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = '0.1.2'
+VERSION = '0.2.0'
 
 
 class VerifyVersionCommand(install):
