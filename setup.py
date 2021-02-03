d9fd7b05838a3409e72855de36903d5c972f0818
eitany <eitan@optibus.com>
2021-02-03 15:18:09 +0200

    indicate python 3 support in classifiers

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setuptools.setup(
     ]},
     classifiers=[
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
