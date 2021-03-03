f383a3a984827f72490a023b1b3c4cd04e419c35
eitany <eitan@optibus.com>
2021-03-03 08:26:50 +0200

    Create playback context for the flask example and use AsyncRecordOnlyTapeCassette as part of the example

--- examples/flask/main.py
+++ examples/flask/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restplus import Api
 
+from examples.flask.playback_context import init_recording_mode
 from examples.flask.web_services import ContentLengthEndpoint, ContentFirstCharsEndpoint
 
 app = Flask(__name__)
@@ -10,4 +11,5 @@ api.add_resource(ContentFirstCharsEndpoint, '/content_first_chars')
 
 
 if __name__ == '__main__':
+    init_recording_mode()
     app.run(host='0.0.0.0', port=5000, debug=True)
