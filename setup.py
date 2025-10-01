ed7cc0b871f307a16144b5b4a305cf2ee82e5b67
Tymoteusz Dzienniak <tymoteusz.dzienniak@optibus.com>
2025-10-01 10:50:26 +0200

    [ci skip] bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.install import install
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = '0.3.28'
+VERSION = '0.3.29'
 
 
 class VerifyVersionCommand(install):
