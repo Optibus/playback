569b46d7a16f52d868fb92728f2820d6217d0f17
michaelmasin <michael.masin@optibus.com>
2026-02-26 13:55:18 +0200

    [ci skip] bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.install import install
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = '0.3.34'
+VERSION = '0.3.35'
 
 
 class VerifyVersionCommand(install):
